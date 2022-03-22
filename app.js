bc63999b43a465bd9b88e4c4d8123ffc59ac77e5
Audrey Jo <audreyjo35@gmail.com>
2022-03-22 11:33:38 +0900

    render index.html, not index.jade

---
+++ app.js
@@ -0,0 +1,52 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+
+// for res.render to render .html rather than default .jade
+// handles Error: No default engine was specified and no extension was provided.
+// https://stackoverflow.com/a/23596000
+var hbs = require('hbs');
+app.set('view engine', 'html');
+app.engine('html', hbs.__express);
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
